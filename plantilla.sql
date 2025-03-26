9c91fd058ddeba077090e425a042bbb295ff26b4
EstefaniaRubiano <rubianoestefania15@gmail.com>
2025-03-26 10:08:11 +0100

    plantilla

---
+++ plantilla.sql
@@ -0,0 +1,24 @@
+/* *****************************************************
+  INSTITUT TIC de Barcelona
+    CFGS _____________________________
+    Mòdul: 0484 Bases de dades. 
+    AUTORS: _______________________________
+    DATA: ________________
+****************************************************** */
+
+-- Pregunta 1
+select 'No ho sé';
+
+-- Pregunta 2
+select 'No ho sé';
+
+-- Pregunta 3
+select 'No ho sé';
+
+-- Pregunta 4
+select 'No ho sé';
+
+-- Pregunta 5
+select 'No ho sé';
+
+
